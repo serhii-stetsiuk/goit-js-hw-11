69664763adef6af43a6214ce68bb81b36c7104a7
SERHII STETSIUK <skystets@gmail.com>
2025-08-14 10:06:54 +0200

    u

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ const inputText = document.querySelector('.input_text');
 
 allForm.addEventListener('submit', (e) => {
 	e.preventDefault();
-	if (!inputText.value) {
+	if (!inputText.value.trim()) {
 		 iziToast.error({
 			position: 'topRight',
 			message: 'Sorry, there are no images matching your search query. Please try again!',
